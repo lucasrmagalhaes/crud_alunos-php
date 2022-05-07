04aec5d39c8907f2eb3bc811ab7b22c4a6bc28af
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-05-07 15:42:34 -0300

    add: formato jpg

--- app/Views/pages/aluno.php
+++ app/Views/pages/aluno.php
@@ -1,11 +1,7 @@
 <div class="container bg-red">
     <div class="card my-3">
-        <div class="card-body d-flex justify-content-center">
-            <img 
-                src="<?= $alunos['img'] ?>" 
-                alt="Aluno"
-                class="img-fluid col-md-3"
-            >
+        <div class="card-body d-flex justify-content-center">    
+            <img src='data:image/jpg;base64,<?= $alunos['img'] ?>' alt='Aluno' class='img-fluid col-md-3'>
         </div>
 
         <div class="card-footer d-flex justify-content-end">
