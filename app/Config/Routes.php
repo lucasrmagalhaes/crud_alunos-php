3d321c46cdd982a7488dd5b2ae993bcf66289cdd
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-05-06 21:05:31 -0300

    add: gitkeep

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,18 +9,18 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
 }
 
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Alunos');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 $routes->setAutoRoute(true);
 
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Alunos::index');
 $routes->match(['get', 'post'], 'alunos/inserir', 'Alunos::inserir');
 $routes->match(['get', 'post'], 'alunos/gravar', 'Alunos::gravar');
 $routes->match(['get', 'post'], 'alunos/editar/(:num)', 'Alunos::editar/$1');
 $routes->match(['get', 'post'], 'alunos/excluir/(:num)', 'Alunos::excluir/$1');
-$routes->get('alunos', 'Alunos::index');
+// $routes->get('alunos', 'Alunos::index');
 $routes->get('alunos/(:segment)', 'Alunos::item/$1');
 
 if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
